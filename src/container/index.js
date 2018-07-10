afa11ac429d72ff5191f0b7ffd1014b5dcf0e4cb
Daniel Sidabutar <aguspermadi94@gmail.com>
2018-07-11 01:25:41 +0700

    displaying error message

--- src/container/index.js
+++ src/container/index.js
@@ -6,10 +6,12 @@ import {
   Form,
   Label,
   Input,
-  Button
+  Button,
+  FormFeedback
 } from 'reactstrap';
 
 import { isEmailFormat } from '../utils';
+import ModalSuccess from './Modal';
 
 class HomePage extends React.Component {
   constructor(props) {
@@ -17,27 +19,36 @@ class HomePage extends React.Component {
     this.state = {
       name: '',
       email: '',
-      phoneCode: '',
+      phoneCode: '62',
       phoneNumber: '',
       address: '',
       isSubscribe: true,
       _error: null,
-      listCode: ['62', '46', '65', '86']
+      listCode: ['62', '46', '65', '86'],
+      isDone: false
     }
     this.handleChangeText = this.handleChangeText.bind(this);
     this.handleChecked = this.handleChecked.bind(this);
     this.onSubmitData = this.onSubmitData.bind(this);
     this.formValidation = this.formValidation.bind(this);
+    this.onToggleModal = this.onToggleModal.bind(this);
+    this.onClearToggle = this.onClearToggle.bind(this);
   }
 
   formValidation() {
     let errors = {};
-    if (this.state.name.length <= 5) 
-      errors['name'] = ['Name must be min 5 character'];
+    if (this.state.name === '')
+      errors['name'] = errors['name'] !== undefined ? [ ...errors.name, 'Please type your name'] : ['Please type your name'];
+    if (this.state.name.length < 5) 
+      errors['name'] = errors['name'] !== undefined ? [ ...errors.name, 'Name must be min 5 character'] : ['Name must be min 5 character'];
     if (this.state.email === '')
       errors['email'] = errors['email'] !== undefined ? [ ...errors.email, 'Please type your email'] : ['Please type your email'];
     if (!isEmailFormat(this.state.email)) 
       errors['email'] = errors['email'] !== undefined ? [ ...errors.email, 'Your email format is wrong'] : ['Your email format is wrong'];
+    if (this.state.phoneNumber === '')
+      errors['phoneNumber'] = ['Please type your phone number']
+    if (this.state.address.length > 130)
+      errors['address'] = ['Maximum number of characters 130']
     if (Object.keys(errors).length) {
       this.setState({
         _error: {
@@ -60,6 +71,26 @@ class HomePage extends React.Component {
     })
   }
 
+  onToggleModal() {
+    this.setState({
+      isDone: !this.state.isDone
+    })
+  }
+
+  onClearToggle() {
+    this.setState({
+      name: '',
+      email: '',
+      phoneCode: '62',
+      phoneNumber: '',
+      address: '',
+      isSubscribe: true,
+      _error: null,
+      listCode: ['62', '46', '65', '86'],
+      isDone: false
+    })
+  }
+ 
   handleChangeOption = (e) => {
     const lastOption = this.state.listCode;
     const indexItem =  e.target.options.selectedIndex;
@@ -71,15 +102,36 @@ class HomePage extends React.Component {
 
   onSubmitData(e) {
     e.preventDefault();
-    const valid = this.formValidation();
-    console.log('VALIDateee ', valid);
+    const isComplete = this.formValidation();
+    if (Object.keys(isComplete).length===0) {
+      this.onToggleModal();
+    }
+  }
+
+  errorChecker(name) {
+    if(this.state._error!==null){
+      const hasil = Object.keys(this.state._error.errors).filter(err => err === name).length > 0;
+      console.log('HASIL ', hasil, ' --- ', this.state._error)
+      return Object.keys(this.state._error.errors).filter(err => err === name).length > 0;
+    }
+    return false;
+  }
+
+  renderFeedBack(message) {
+    return <p style={styles.feedBack}>{message}</p>
   }
 
   render() {
     console.log('THIS STATE ', this.state);
     return (
       <Container style={styles.container}>
-        <h3>JS Function</h3>
+        <ModalSuccess 
+          isOpen={this.state.isDone}
+          onToggle={this.onToggleModal}
+          onClearToggle={this.onClearToggle}
+          email={this.state.email}
+        />
+        <h3 onClick={() => this.errorChecker('name')}>JS Function</h3>
         <Form onSubmit={this.onSubmitData}>
           <Row style={styles.row}>
             <Col md={2} style={styles.titleItem}>
@@ -87,11 +139,16 @@ class HomePage extends React.Component {
             </Col>
             <Col md={10}>
               <Input 
+                invalid={this.errorChecker('name')}
                 onChange={this.handleChangeText} 
                 type="text" 
                 name="name" 
                 value={this.state.name}
               />
+                {
+                  !this.errorChecker('name') ? null :
+                  this.state._error.errors.name.map(item => this.renderFeedBack(item))
+                }
             </Col>
           </Row>
           <Row style={styles.row}>
@@ -100,10 +157,16 @@ class HomePage extends React.Component {
             </Col>
             <Col md={10}>
               <Input 
+                invalid={this.errorChecker('email')}
                 onChange={this.handleChangeText} 
-                type="email" name="email" 
+                type="text" 
+                name="email" 
                 value={this.state.email}
               />
+                {
+                  !this.errorChecker('email') ? null :
+                  this.state._error.errors.email.map(item => this.renderFeedBack(item))
+                }
             </Col>
           </Row>
           <Row style={styles.row}>
@@ -126,10 +189,15 @@ class HomePage extends React.Component {
                 </Col>
                 <Col md={10}>
                   <Input 
+                    onChange={this.handleChangeText}
                     type="number" 
                     name="phoneNumber" 
                     value={this.state.phoneNumber}
                   />
+                    {
+                      !this.errorChecker('phoneNumber') ? null :
+                      this.state._error.errors.phoneNumber.map(item => this.renderFeedBack(item))
+                    }
                 </Col>
               </Row>
             </Col>
@@ -140,6 +208,7 @@ class HomePage extends React.Component {
             </Col>
             <Col md={10}>
               <Input 
+                onChange={this.handleChangeText}
                 type="textarea" 
                 size="130" 
                 maxLength={130} 
@@ -193,6 +262,15 @@ const styles = {
   },
   titleItem: {
     textAlign: 'right'
+  },
+  feedBack: {
+    margin: '2px 0px',
+    textAlign: 'left',
+    color: '#fff',
+    background: '#e5616b',
+    display: 'table',
+    padding: '0px 11px',
+    borderRadius: '3px',
   }
 }
 
