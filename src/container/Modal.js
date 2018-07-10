7f3fdf43ec29a4322cd65c692d9fbd6120744063
Daniel Sidabutar <aguspermadi94@gmail.com>
2018-07-11 01:44:18 +0700

    pass some props to modal success

--- src/container/Modal.js
+++ src/container/Modal.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import { Modal, ModalHeader, ModalBody, Button, ModalFooter } from 'reactstrap';
+import { Modal, ModalHeader, ModalBody, Button, ModalFooter, Alert } from 'reactstrap';
+import { generateInvoices } from '../utils';
 
-const ModalSucces = (props) => {
-  console.log('PROPS MODAL', props);
-  return (
-    <Modal isOpen={props.isOpen} toggle={props.onToggle}>
-      <ModalHeader toggle={props.onToggle}>Thank You!</ModalHeader>
-      <ModalBody>
-        Your ticket has sent to email {props.email}
-      </ModalBody>
-      <ModalFooter>
-        <Button color="secondary" onClick={props.onClearToggle}>OK</Button>
-      </ModalFooter>
-    </Modal>
-  )
-}
+const ModalSucces = (props) => (
+  <Modal isOpen={props.isOpen} toggle={props.onClearToggle}>
+    <ModalHeader toggle={props.onClearToggle}>Thank You!</ModalHeader>
+    <ModalBody>
+      Your ticket has sent to email <strong>{props.email}</strong>
+      <Alert>Invoices :  { generateInvoices(6) }</Alert>
+    </ModalBody>
+    <ModalFooter>
+      <Button color="secondary" onClick={props.onClearToggle}>OK</Button>
+    </ModalFooter>
+  </Modal>
+);
+
 export default ModalSucces;
