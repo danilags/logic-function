0a7e5a7a66160310e79ef15da8993f9a2779bfee
Daniel Sidabutar <aguspermadi94@gmail.com>
2018-07-11 01:46:44 +0700

    adding new function to generate random invoices alphanumeric

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,1 +1,10 @@
 export const isEmailFormat = str => (/^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/).test(str);
+
+export const generateInvoices = (count) => {
+  let path = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let result = '';
+  for (let i = 0; i < count; i++) {
+      result += path[Math.floor(Math.random() * path.length)];
+  }
+  return result;
+}
